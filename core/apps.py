f79ac9605815362a1cc71fee332ee6bab818232f
vinsaintmw <slimdj@hotmail.com>
2026-04-20 20:43:35 +0200

    fix superuser creation

--- core/apps.py
+++ core/apps.py
@@ -1,7 +1,18 @@
 from django.apps import AppConfig
 
-
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
-    verbose_name = 'School Management Core'
+
+    def ready(self):
+        import os
+        from django.contrib.auth import get_user_model
+
+        if os.environ.get("CREATE_SUPERUSER") == "True":
+            User = get_user_model()
+            if not User.objects.filter(username="admin").exists():
+                User.objects.create_superuser(
+                    username="admin",
+                    email="admin@example.com",
+                    password="admin123"
+                )
