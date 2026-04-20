cc4bf90940aab95e60d29b57c9fc508da18be311
vinsaintmw <slimdj@hotmail.com>
2026-04-20 21:12:35 +0200

    create superuser

--- High_Profile/settings.py
+++ High_Profile/settings.py
@@ -167,13 +167,3 @@ X_FRAME_OPTIONS = 'DENY'
 
 SECURE_CONTENT_TYPE_NOSNIFF = True
 
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser(
-        username="admin",
-        email="admin@example.com",
-        password="admin123"
-    )
--- core/apps.py
+++ core/apps.py
@@ -8,11 +8,4 @@ class CoreConfig(AppConfig):
         import os
         from django.contrib.auth import get_user_model
 
-        if os.environ.get("CREATE_SUPERUSER") == "True":
-            User = get_user_model()
-            if not User.objects.filter(username="admin").exists():
-                User.objects.create_superuser(
-                    username="admin",
-                    email="admin@example.com",
-                    password="admin123"
-                )
+       
